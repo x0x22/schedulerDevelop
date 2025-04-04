32aadaf44776e1f010cbafa79ca5728dd942d934
isom <smallsol.fa@gmail.com>
2025-04-04 11:17:19 +0900

    비밀번호 변경 api

--- src/main/java/com/study/scheduler/repository/UserRepository.java
+++ src/main/java/com/study/scheduler/repository/UserRepository.java
@@ -2,9 +2,22 @@ package com.study.scheduler.repository;
 
 import com.study.scheduler.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
+
+    List<User> id(Long id);
+
+    Optional<User> findByUserid(String userid);
+
+    default User findByUserIdOrElseThrow(String userid){
+        return findByUserid(userid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + userid));
+    }
 
 }
